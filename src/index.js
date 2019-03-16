2f97db6012da2b9ee6cacde3f511a74d034fe2c6
jubairmizan <jubairmizan@gmail.com>
2019-03-16 13:01:15 +0600

    Api Integrated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends React.Component{
+	constructor(props){
+		super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+		axios.get('http://gonona-lab.com/api/api/topCardNews')
+		  .then(function (response) {
+		    // handle success
+		    console.log(response);
+		  })
+		  .catch(function (error) {
+		    // handle error
+		    console.log(error);
+		  })
+		  .then(function () {
+		    // always executed
+		  });
+
+
+	}
+	render(){
+		return(
+			<div className="container">
+				<p>Lorem Ipsum Dolor Set Amet</p>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Index />, document.querySelector('#root'));
