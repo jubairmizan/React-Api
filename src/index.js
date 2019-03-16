f23fd020bc765ffe69c4ca7b5a93fdfcf67153b6
jubairmizan <jubairmizan@gmail.com>
2019-03-16 13:45:14 +0600

    api integrate with async and await and jsonStringfy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class Index extends React.Component{
 		axios.get('http://gonona-lab.com/api/api/topCardNews')
 		.then(function (response) {
 			// handle success
-			console.log(response);
+			console.log('rrr'+response);
 		})
 		.catch(function (error) {
 			// handle error
@@ -21,10 +21,19 @@ class Index extends React.Component{
 
 
 	}
+
+	async apiLoad(){
+		const response = await axios.get('https://hn.algolia.com/api/v1/search');
+
+		console.log('Response Data will load here '+ JSON.stringify(response.data.hits.length));
+	}
+
 	render(){
 		return(
 			<div className="container">
 				<p>Lorem Ipsum Dolor Set Amet</p>
+
+				<button onClick={this.apiLoad}>Click to get Api Data</button>
 			</div>
 		);
 	}
