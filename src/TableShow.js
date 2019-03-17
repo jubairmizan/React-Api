70b87cc252d88da694b9d7a8e26bfe3263784770
jubairmizan <jubairmizan@gmail.com>
2019-03-17 14:11:20 +0600

    Api integrate with view table

---
+++ src/TableShow.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const TableShow = (props) => {
+	const rowsData = props.tableData.map((rowData,index)=>{
+		return <tr key={index}><td>{index+1}</td><td>{rowData.title}</td><td>{rowData.url}</td><td>{rowData.author}</td><td>{rowData.points}</td><td>{rowData.num_comments}</td><td>{rowData.created_at}</td></tr>;
+	});
+
+	return (
+		<div className="table-responsive">
+			<table className="table table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>Sl</th>
+						<th>Title</th>
+						<th>Url</th>
+						<th>Author</th>
+						<th>Points</th>
+						<th>Number of comments</th>
+						<th>Created At</th>
+					</tr>
+				</thead>
+				<tbody>
+					{rowsData}
+				</tbody>
+			</table>
+		</div> 
+	);
+}
+
+export default TableShow;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import TableShow from './TableShow';
 
 class Index extends React.Component{
-	constructor(props){
-		super(props);
+	state = {apiData:[]};
 
-		axios.get('http://gonona-lab.com/api/api/topCardNews')
-		.then(function (response) {
-			// handle success
-			console.log('rrr'+response);
-		})
-		.catch(function (error) {
-			// handle error
-			console.log(error);
-		})
-		.then(function () {
-			// always executed
-		});
-
-
-	}
-
-	async apiLoad(){
+	apiLoad = async () =>{
 		const response = await axios.get('https://hn.algolia.com/api/v1/search');
-
-		console.log('Response Data will load here '+ JSON.stringify(response.data.hits.length));
+		// console.log('Response Data will load here '+ JSON.stringify(response.data.hits));
+		
+		this.setState({apiData:response.data.hits});
+	}
+	apiReset = async () =>{
+		this.setState({apiData:[]});
 	}
 
 	render(){
+		let tableLength;
+		let tableShow;
+
+		if(this.state.apiData.length>0){
+			tableLength = <p style={{ marginTop:'20px' }}>Data Found : {this.state.apiData.length}</p>;
+			
+			tableShow = <TableShow tableData={this.state.apiData} />;
+		}else{
+			tableLength = <p style={{ marginTop:'20px' }}> No Data Found </p>;
+		}
+
 		return(
 			<div className="container">
-				<p>Lorem Ipsum Dolor Set Amet</p>
+				<div className="row">
+					<div className="col-md-12">
+						<p>Click the button and see the api lists of table</p>
 
-				<button onClick={this.apiLoad}>Click to get Api Data</button>
+						<button className="btn btn-info" onClick={this.apiLoad}>Click to get Api Data</button>
+						<button className="btn btn-warning" style={{ marginLeft:'10px' }} onClick={this.apiReset}>Reset</button>
+					</div>
+
+					<div className="col-md-12">
+						{tableLength}
+						{tableShow}
+					</div>
+				</div>
 			</div>
 		);
 	}
